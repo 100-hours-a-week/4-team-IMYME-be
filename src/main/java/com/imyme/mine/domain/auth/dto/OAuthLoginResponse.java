e7e925fcd357822539c5138961a3f1a429cc3caa
JINOJ37 <jinoj0423@gmail.com>
2026-01-21 14:45:36 +0900

    ✨ Feat : 로그인 요청 및 응답 DTO 생성
    OAuthLoginRequest, OauthResponse 생성

---
+++ src/main/java/com/imyme/mine/domain/auth/dto/OAuthLoginResponse.java
@@ -0,0 +1,36 @@
+package com.imyme.mine.domain.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * OAuth 로그인 응답 DTO
+ */
+@Getter
+@Builder
+@AllArgsConstructor
+public class OAuthLoginResponse {
+
+    private String accessToken;
+    private String refreshToken;
+    private Long deviceId;
+    private UserInfo user;
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class UserInfo {
+        private Long id;
+        private String oauthId;
+        private String oauthProvider;
+        private String nickname;
+        private String profileImageUrl;
+        private Integer level;
+        private Integer totalCardCount;
+        private Integer activeCardCount;
+        private Integer consecutiveDays;
+        private Integer winCount;
+        private Boolean isNewUser;
+    }
+}
